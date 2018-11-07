eff2ec0dc27a3a465f89b749cf716164c935e21b
pdl <pdl@192.168.101.31>
2018-11-07 20:39:04 +0100

    initial commit

---
+++ websocket-test/src/main/java/io/sprachvoll/websocket_test/App.java
@@ -0,0 +1,16 @@
+package io.sprachvoll.websocket_test;
+
+import io.sprachvoll.websocket_test.small.Huge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Huge h = new Huge();
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ websocket-test/src/main/java/io/sprachvoll/websocket_test/small/Huge.java
@@ -0,0 +1,5 @@
+package io.sprachvoll.websocket_test.small;
+
+public final class Huge {
+
+}
